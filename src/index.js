99871d86689bef901b2855930a34bb58850ad0dd
Damon McMinn <damon.mcminn@ticketmaster.co.uk>
2015-09-12 09:19:30 +0100

    Add tests; provide flag for callbacks with arbitrary number of args

---
+++ src/index.js
@@ -0,0 +1,23 @@
+export function fail(next) {
+
+  return function(err) {
+    next(err);
+  }
+
+}
+
+export function success(next, HAS_MULTIPLE_ARGS) {
+
+  // sometimes callbacks take three args
+  return function(result) {
+
+    // if HAS_MULTIPLE_ARGS flag:
+    // create array [error, a, b...]
+    // call next with spreading array
+    return HAS_MULTIPLE_ARGS ? next(...[null, ...result]) : next(null, result);
+
+  }
+
+}
+
+export const HAS_MULTIPLE_ARGS = true;
---
+++ test/main.js
@@ -0,0 +1,64 @@
+import * as depromise from '../src';
+
+describe('depromise#fail', _ => {
+
+  it('should return a function', () => {
+
+    expect(depromise.fail()).to.be.a.function();
+
+  });
+
+  it('returned function should callback with error', done => {
+
+    let error = new Error();
+
+    function callback(err) {
+      expect(err).to.exist();
+      done();
+    }
+
+    let failure = depromise.fail(callback);
+
+    failure(error);
+
+  });
+
+});
+
+describe('depromise#success', () => {
+
+  it('should return a function', () => {
+
+    expect(depromise.success()).to.be.a.function();
+
+  });
+
+  it('returned function should callback (null, result)', () => {
+
+    function callback(err, result) {
+      expect(err).to.be.null();
+      expect(result).to.be.true();
+    }
+
+    let success = depromise.success(callback);
+
+    success(true);
+
+  });
+
+  it('returned function should callback (null, second, third, ...) if provided HAS_MULTIPLE_ARGS flag', () => {
+
+    function callback(err, second, third, fourth, fifth) {
+      expect(err).to.be.null();
+      expect(second).to.be.a.string('second');
+      expect(third).to.be.a.string('third');
+      expect(fifth).to.exist();
+    }
+
+    let success = depromise.success(callback, depromise.HAS_MULTIPLE_ARGS);
+
+    success(['second', 'third', 'fourth', 'fifth']);
+
+  });
+
+});
